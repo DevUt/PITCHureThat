e93cd90ab6df9d98f5a3f9713b5824e498593d25
rishabhrao <rishabhraos1@gmail.com>
2021-09-12 00:13:07 +0530

    Initialize PITCHureThat with flutter
    Signed-off-by: rishabhrao <rishabhraos1@gmail.com>

---
+++ android/app/src/main/java/rishabhrao/pitchurethat/pitchure_that/MainActivity.java
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * Copyright © 2021 Rishabh Rao.
+ * All Rights Reserved.
+ *
+ ******************************************************************************/
+
+package rishabhrao.pitchurethat.pitchure_that;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
